63199c25f1995bf84be52ab06c4c636f31818310
Carlo Rodrigo Diaz Portilla <cdiazportillap@gmail.com>
2026-02-09 11:05:25 -0500

    feat: add AuthRouter

---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AuthController } from "./controller";
+
+export class AuthRouter {
+  static get routes() {
+    const router = Router();
+    const controller = new AuthController();
+
+    router.post("/login", controller.login);
+    router.post("/register", controller.register);
+    router.get("/validate-email/:token", controller.validateEmail);
+
+    return router;
+  }
+}
--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { AuthRouter } from "./auth/routes";
 
 export class AppRouter {
   static get routes(): Router {
     const router = Router();
+
+    router.use("/api/auth", AuthRouter.routes);
 
     return router;
   }
